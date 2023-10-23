2faf72c8954bc0c44783f5417045fa3c095d1117
MiataBoy <joshua@intermixmanagement.nl>
2023-10-23 16:19:16 +0200

    homepage

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>JDYata home</title>
+        <meta name="og:title" content="JDYata home">
+        <meta name="og:description" content="JDYata, the ultimate hub for all your Miata facts, knowledge and pictures">
+        <meta name="og:image" content="/resources/assets/miata.jpeg">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="resources/style/homePage.css">
+
+        <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <?php include 'resources/components/navbar.html'?>
+        <div class="container">
+            <div class="main-container">
+                <img class='body-logo' src="resources/assets/miata.jpeg" alt="JDYata logo">
+                <p>JDYata is the source for all your mazda mx-5 miata facts, knowledge, pictures and memes. Made by passionate people, for passionate people.
+                    Our heart and love has been poured into this website, and we hope you enjoy the content as much as we enjoyed making it for you.
+                    Is your content on here, without your consent or will? Contact <a class="link" href="mailto:support@wolfsvale.tech">support@wolfsvale.tech</a> and we'll get it removed.</p>
+            </div>
+        </div>
+        <p class="body-footer">Website version: 1.0.0-Delta | <a class="link" href="https://wolfsvale.tech" target="_blank">JDYata 2023 &#169; Wolfsvale Software</a></p>
+    </body>
+</html>
